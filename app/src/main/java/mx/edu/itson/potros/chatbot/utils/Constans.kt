1efeacecfcdda1eaba301fc3555359a0fa21431b
Alec Demian Santana Celaya <73375255+SantanaAlec@users.noreply.github.com>
2023-04-25 19:38:45 -0700

    Initial Commit

---
+++ app/src/main/java/mx/edu/itson/potros/chatbot/utils/Constans.kt
@@ -0,0 +1,9 @@
+package mx.edu.itson.potros.chatbot.utils
+
+object Constans {
+    const val SEND_ID="SEND_ID"
+    const val RECEIVE_ID="RECEIVE_ID"
+
+    const val OPEN_GOOGLE="opening Google"
+    const val OPEN_SEARCH="Searching..."
+}
